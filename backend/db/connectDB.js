9d5c6ed8ed261cbc672aafdc352d97adcffac0cf
DeepakKumar0407 <deepak.kumar016211@gmail.com>
2025-07-22 18:20:23 +0530

    Update connectDB.js

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
 	try {
-		const conn = await mongoose.connect("mongodb+srv://Deepak0407:Deepak0407@cluster0.myf7xyr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+		const conn = await mongoose.connect("mongodb+srv://@cluster0.myf7xyr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
 
 		});
 
